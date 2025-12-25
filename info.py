d9da92601b8d70f552f905df0e539d884309b46e
rinoffi <sivaneshoffi@gmail.com>
2025-12-25 15:28:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", ""))
 API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "8308294882:AAHgsIbWuH1imLlkvIxnZuDt7c-K-F4jeYs")
 DATABASE_URI = environ.get("DATABASE_URI", "")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
 ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+CHANNEL      = environ.get("CHANNEL", "")
