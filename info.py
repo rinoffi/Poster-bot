ffedb8d2e5553a0f5995208e78a44529f0541fa8
rinoffi <sivaneshoffi@gmail.com>
2025-12-25 15:33:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,8 +7,8 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "24160099"))
+API_HASH     = environ.get("API_HASH", "381515f2540b0bf817a68af8b352f5b7")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "8308294882:AAHgsIbWuH1imLlkvIxnZuDt7c-K-F4jeYs")
 DATABASE_URI = environ.get("DATABASE_URI", "")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002853893090"))
