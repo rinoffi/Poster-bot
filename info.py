2c649e6032afb5d859832e824b94d68116d925aa
rinoffi <sivaneshoffi@gmail.com>
2025-12-25 15:39:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ from os import environ
 API_ID       = int(environ.get("API_ID", "24160099"))
 API_HASH     = environ.get("API_HASH", "381515f2540b0bf817a68af8b352f5b7")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "8308294882:AAHgsIbWuH1imLlkvIxnZuDt7c-K-F4jeYs")
-DATABASE_URI = environ.get("DATABASE_URI", "")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://haribotx:haribotx@cluster0.i3skil4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002853893090"))
 ADMIN        = int(environ.get("ADMIN", "6248675084"))
 CHANNEL      = environ.get("CHANNEL", "@Techzorotamil")
